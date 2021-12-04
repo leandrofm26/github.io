4ae5f9bf027855e202b18b553e61871bdde38c64
leandrofm26 <95538687+leandrofm26@users.noreply.github.com>
2021-12-04 13:49:56 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,127 @@
+
+    var nam = window.prompt('Qual seu Nome?')
+
+    
+    var mensagem = ('Mensagem de *'  + nam +'* para você  '  ) 
+    document.write("<H2>"+'Olá, ' + nam )  
+    var url1 =  ( '  my-all.top/?' ) 
+    
+    
+       
+    function obterFrase (){
+    var r_text = new Array ();
+    [r_text[0] ="🙏👉pois o Senhor, o seu Deus, os acompanhará e lutará por vocês contra os seus inimigos, para dar a vitória a vocês'. ",
+    r_text[1] =     "🙏👉Não foi pela espada que conquistaram a terra nem pela força do seu braço que alcançaram a vitória; foi pela tua mão direita, pelo teu braço e pela luz do teu rosto,por causa do teu amor para com eles. És tu, meu Rei e meu Deus!És tu que decretas vitórias para Jacó! ",
+    r_text[2] =     "🙏👉Prepara-se o cavalo para o dia da batalha, mas o Senhor é que dá a vitória. Provérbios 21:31",
+    r_text[3] =     "🙏👉Tudo posso naquele que me fortalece. Filipenses 4:13",
+    r_text[4] =     "🙏👉Com Deus conquistaremos a vitória, e ele pisará os nossos adversários. Salmos 108:13 .",
+    r_text[5] =     "🙏👉Pois ainda que o justo caia sete vezes,tornará a erguer-se,mas os ímpios são arrastados pela calamidade. Provérbios 24:16.",
+    r_text[6] =     "🙏👉Eu farei de você uma muralha de bronze fortificada diante deste povo; lutarão contra você,mas não o vencerão,pois estou com você para resgatá-lo e salvá-lo,declara o Senhor. Jeremias 15:20",
+    r_text[7] =     "🙏👉Se trabalhamos e lutamos é porque temos depositado a nossa esperança no Deus vivo, o Salvador de todos os homens, especialmente dos que creem. 1 Timóteo 4:10",
+    r_text[8] =      "🙏👉Alguns confiam em carros e outros em cavalos,mas nós confiamos no nome do Senhor, o nosso Deus. Salmos 20:7.",
+    r_text[9] =      "🙏👉Ninguém conseguirá resistir a você todos os dias da sua vida. Assim como estive com Moisés, estarei com você; nunca o deixarei, nunca o abandonarei.Seja forte e corajoso, porque você conduzirá este povo para herdar a terra que prometi sob juramento aos seus antepassados. Josué 1:5-6",
+    r_text[10] =     "🙏👉Por isso não tema, pois estou com você;não tenha medo, pois sou o seu Deus.Eu o fortalecerei e o ajudarei;eu o segurarei com a minha mão direita vitoriosa. Isaías 41:10",
+    r_text[11] =      "🙏👉Estejam vigilantes, mantenham-se firmes na fé, sejam homens de coragem, sejam fortes. 1 Coríntios 16:13.'",
+    r_text[12] =    "🙏👉Sejam fortes e corajosos. Não tenham medo nem fiquem apavorados por causa delas, pois o Senhor, o seu Deus, vai com vocês; nunca os deixará, nunca os abandonará. Deuteronômio 31:6",
+    r_text[13] =     "🙏👉Pois estou convencido de que nem morte nem vida, nem anjos nem demônios, nem o presente nem o futuro, nem quaisquer poderes, nem altura nem profundidade, nem qualquer outra coisa na criação será capaz de nos separar do amor de Deus que está em Cristo Jesus, nosso Senhor. Romanos 8:38-39",
+    r_text[14] =     "🙏👉Eis que estou à porta e bato. Se alguém ouvir a minha voz e abrir a porta, entrarei e cearei com ele, e ele comigo. Apocalipse 3:20",
+    r_text[15] =     "🙏👉Eu farei de você uma muralha de bronze fortificada diante deste povo; lutarão contra você,mas não o vencerão,pois estou com você para resgatá-lo e salvá-lo,declara o Senhor. Jeremias 15:20",
+    r_text[16] =     "🙏👉Então o Anjo do Senhor apareceu a Gideão e lhe disse: O Senhor está com você, poderoso guerreiro. Juízes 6:12",
+    r_text[17] =     "🙏👉Jesus olhou para eles e respondeu: Para o homem é impossível, mas para Deus todas as coisas são possíveis. Mateus 19:26",
+    r_text[18] =     "🙏👉Jesus respondeu: O que é impossível para os homens é possível para Deus. Lucas 18:27",
+    r_text[19] =    "🙏👉Portanto, eu digo: Tudo o que vocês pedirem em oração, creiam que já o receberam, e assim sucederá. Marcos 11:24",
+    r_text[20] =    "🙏👉Agora, pois, Senhor, que espero eu? A minha esperança está em ti. Salmos 39.7”",
+r_text[21] = "🙏👉Ó minha alma, espera somente em Deus, porque dele vem a minha esperança. Salmos 62.5",
+r_text[22] = "🙏👉Sustenta-me conforme a tua palavra, para que viva, e não me deixes envergonhado da minha esperança. Salmos 119.116",
+r_text[23] = "🙏👉Porque eu bem sei os pensamentos que tenho a vosso respeito, diz o Senhor; pensamentos de paz, e não de mal, para vos dar o fim que esperais. Jeremias 29.11",
+r_text[24] = "🙏👉Agora, pois, Senhor, o que espero eu? A minha esperança está em ti. Salmos 39.7",
+r_text[25] = "🙏👉Por cuja causa padeço também isto, mas não me envergonho; porque eu sei em quem tenho crido, e estou certo de que é poderoso para guardar o meu depósito até àquele dia. 2 Timóteo 1.12",
+r_text[26] = "🙏👉E não somente isto, mas também nos gloriamos nas tribulações; sabendo que a tribulação produz a paciência, e a paciência, a experiência, e a experiência a esperança. Romanos 5.4",
+r_text[27] = "🙏👉Porque o Senhor será a tua esperança; guardará os teus pés de serem capturados. Provérbios 3.26",
+r_text[28] = "🙏👉Então Samuel tomou uma pedra, e a pôs entre Mizpá e Sem, e lhe chamou Ebenézer; e disse. Até aqui nós ajudou o Senhor. 1 Samuel 7.12",
+r_text[29] = "🙏👉Porque, qual é a nossa esperança, ou gozo, ou coroa de glória? Porventura não sois vós também diante de nosso Senhor Jesus Cristo em sua vinda? 1 Tessalonicenses 2.19",
+r_text[30] = "🙏👉Tudo o que fizerem, façam de todo o coração, como para o Senhor, e não para os homens. Colossenses 3.23",
+r_text[31] = "🙏👉Nossa esperança está no Senhor; ele é o nosso auxílio e a nossa proteção. Salmo 33.20",
+r_text[32] = "🙏👉Irmãos, quanto a mim, não julgo que o haja alcançado; mas uma coisa faço, e é que, esquecendo-me das coisas que atrás ficam, e avançando para as que estão diante de mim, Prossigo para o alvo, pelo prêmio da soberana vocação de Deus em Cristo Jesus. Filipenses 3.13,14",
+r_text[33] = "🙏👉E a esperança não traz confusão, porquanto o amor de Deus está derramado em nossos corações pelo Espírito Santo que nos foi dado. Romanos 5.5",
+r_text[34] = "🙏👉Esperei com paciência no SENHOR, e ele se inclinou para mim, e ouviu o meu clamor. Salmos 40.1",
+r_text[35] = "🙏👉Sejam fortes e corajosos. Não tenham medo nem fiquem apavorados por causa delas, pois o Senhor, o seu Deus, vai com vocês; nunca os deixará, nunca os abandonará”. Deuteronômio 31.6",
+r_text[36] = "🙏👉Sustenta-me conforme a tua palavra, para que viva, e não me deixes envergonhado da minha esperança. Salmos 119.116",
+r_text[37] = "🙏👉Antes, santificai ao Senhor Deus em vossos corações; e estai sempre preparado para responder com mansidão e temor a qualquer que vos pedir a razão da esperança que há em vós. 1 Pedro 3.15",
+r_text[38] = "🙏👉Guia-me na tua verdade, e ensina-me, pois tu és o Deus da minha salvação; por ti estou esperando todo o dia. Salmos 25.5",
+r_text[39] = "🙏👉Alegrai-vos na esperança, sede pacientes na tribulação, perseverai na oração; Romanos 12.12",
+r_text[40] = "🙏👉Lembrando-nos sem cessar da obra da vossa fé, do trabalho do amor, e da paciência da esperança em nosso Senhor Jesus Cristo, diante de nosso Deus e Pai, 1 Tessalonicenses 1.3",
+r_text[41] = "🙏👉Não quero, porém, irmãos, que sejais ignorantes acerca dos que já dormem, para que não vos entristeçais, como os demais, que não têm esperança. 1 Tessalonicenses 4.13",
+r_text[42] = "🙏👉Tu, Senhor, manténs acesa a minha lâmpada; o meu Deus transforma em luz as minhas trevas. Salmo 18.28",
+r_text[43] = "🙏👉Agora, pois, permanecem a fé, a esperança e o amor, estes três, mas o maior destes é o amor. 1 Coríntios 13.13",
+r_text[44] = "🙏👉Porque eu sei que o meu Redentor vive, e que por fim se levantará sobre a terra. Jó 19.25",
+r_text[45] = "🙏👉Mas nós, que somos do dia, sejamos sóbrios, vestindo-nos da couraça da fé e do amor, e tendo por capacete a esperança da salvação; 1 Tessalonicenses 5.8",
+r_text[46] = "🙏👉Segundo a minha intensa expectação e esperança, de que em nada serei confundido; antes, com toda a confiança, Cristo será, tanto agora como sempre, engrandecido no meu corpo, seja pela vida, seja pela morte. Filipenses 1.20",
+r_text[47] = "🙏👉Versiculos de esperanças cuidado de Deus",
+r_text[48] = "🙏👉Não temas, porque eu sou contigo; não te assombres, porque eu sou teu Deus; eu te fortaleço, e te ajudo, e te sustento com a destra da minha justiça. Isaías 41.10",
+r_text[49] = "🙏👉Na verdade, na verdade vos digo que quem ouve a minha palavra, e crê naquele que me enviou, tem a vida eterna, e não entrará em condenação, mas passou da morte para a vida. João 5.24",
+r_text[50] = "🙏👉Ó minha alma, espera somente em Deus, porque dele vem a minha esperança. Salmos 62.5",
+r_text[51] = "🙏👉E tudo quanto pedirdes em meu nome eu o farei, para que o Pai seja glorificado no Filho. Se pedirdes alguma coisa em meu nome, eu o farei. João 14.13,14",
+r_text[52] = "🙏👉Não andem ansiosos por coisa alguma, mas em tudo, pela oração e súplicas, e com ação de graças, apresentem seus pedidos a Deus. Filipenses 4.6",
+r_text[53] = "🙏👉Não busquem ansiosamente o que hão de comer ou beber; não se preocupem com isso. Pois o mundo pagão é que corre atrás dessas coisas; mas o Pai sabe que vocês precisam delas. Busquem, pois, o Reino de Deus, e essas coisas lhes serão acrescentadas. Lucas 12.29-31",
+r_text[54] = "🙏👉Em esperança da vida eterna, a qual Deus, que não pode mentir, prometeu antes dos tempos dos séculos. Tito 1.2",
+r_text[55] = "🙏👉Mas Cristo, como Filho, sobre a sua própria casa; a qual casa somos, se tão somente conservarmos firme a confiança e a glória da esperança até ao fim. Hebreus 3.6",
+r_text[56] = "🙏👉Ah! nosso Deus, porventura não os julgarás? Porque em nós não há força perante esta grande multidão que vem contra nós, e não sabemos o que faremos; porém os nossos olhos estão postos em ti. 2 Crônicas 20.12",
+r_text[57] = "🙏👉Bem-aventurado aquele que tem o Deus de Jacó por seu auxílio, e cuja esperança está posta no Senhor seu Deus. Salmos 146.5",
+r_text[58] = "🙏👉Porventura não é também que repartas o teu pão com o faminto, e recolhas em casa os pobres abandonados; e, quando vires o nu, o cubras, e não te escondas da tua carne? Isaías 58.7",
+r_text[59] = "🙏👉Retenhamos firmes a confissão da nossa esperança; porque fiel é o que prometeu. Hebreus 10.23",
+r_text[60] = "🙏👉Quando passares pelas águas estarei contigo, e quando pelos rios, eles não te submergirão; quando passares pelo fogo, não te queimarás, nem a chama arderá em ti. Porque eu sou o Senhor teu Deus. Isaías 43.2,3",
+r_text[61] = "🙏👉Ainda que o meu pai e a minha mãe me abandonem, o Senhor cuidará de mim. Salmos 27.10",
+r_text[62] = "🙏👉Assim será para a tua alma o conhecimento da sabedoria; se a achares, haverá galardão para ti e não será cortada a tua esperança. Provérbios 24.14",
+r_text[63] = "🙏👉Ora o Deus de esperança vos encha de todo o gozo e paz em crença, para que abundeis em esperança pela virtude do Espírito Santo. Romanos 15.13",
+r_text[64] = "🙏👉Ainda que eu ande pelo vale da sombra da morte, não temerei mal algum, pois tu estás comigo. Salmo 23.4",
+r_text[65] = "🙏👉Aguardando a bem-aventurada esperança e o aparecimento da glória do grande Deus e nosso Senhor Jesus Cristo; Tito 2.13",
+r_text[66] = "🙏👉O SENHOR é a minha luz e a minha salvação; a quem temerei? O SENHOR é a força da minha vida; de quem me recearei? Salmos 27.1",
+r_text[67] = "🙏👉Sem fé é impossível agradar a Deus, pois quem dele se aproxima precisa crer que ele existe e que recompensa aqueles que o buscam. Hebreus 11.6",
+r_text[68] = "🙏👉Pois tu és a minha esperança, Senhor Deus; tu és a minha confiança desde a minha mocidade. Salmos 71.5",
+r_text[69] = "🙏👉Bom é ter esperança, e aguardar em silêncio a salvação do Senhor. Lamentações 3.26",
+r_text[70] = "🙏👉Com coisas tremendas em justiça nos responderás, ó Deus da nossa salvação; tu és a esperança de todas as extremidades da terra, e daqueles que estão longe sobre o mar.Salmos 65.5",
+r_text[71] = "🙏👉Porque em esperança fomos salvos. Ora a esperança que se vê não é esperança; porque o que alguém vê como o esperará? Romanos 8.24",
+r_text[72] = "🙏👉Porque nós pelo Espírito da fé aguardamos a esperança da justiça. Gálatas 5.5",
+r_text[73] = "🙏👉Que diremos, pois, a estas coisas? Se Deus é por nós, quem será contra nós? Romanos 8.31",
+r_text[74] = "🙏👉E terás confiança, porque haverá esperança; olharás em volta e repousarás seguro. Jó 11.18",
+r_text[77] = "🙏👉Porventura não é o teu temor de Deus a tua confiança, e a tua esperança a integridade dos teus caminhos? Jó 4.6",
+r_text[78] = "🙏👉Para que, sendo justificados pela sua graça, sejamos feitos herdeiros segundo a esperança da vida eterna. Tito 3.7",
+r_text[79] = "🙏👉E qualquer que nele tem esta esperança purifica-se a si mesmo, como também ele é puro. 1 João 3.3",
+r_text[80] = "🙏👉Pelo qual também temos entrada pela fé a esta graça, na qual estamos firmes, e nos gloriamos na esperança da glória de Deus. Romanos 5.2",
+r_text[81] = "🙏👉Porque tudo o que dantes foi escrito, para nosso ensino foi escrito, para que pela paciência e consolação das Escrituras tenhamos esperança. Romanos 15.4",
+r_text[82] = "🙏👉Para que por duas coisas imutáveis, nas quais é impossível que Deus minta, tenhamos a firme consolação, nós, os que pomos o nosso refúgio em reter a esperança proposta; Hebreus 6.18",
+r_text[83] = "🙏👉Bendito seja o Deus e Pai de nosso Senhor Jesus Cristo que, segundo a sua grande misericórdia, nos gerou de novo para uma viva esperança, pela ressurreição de Jesus Cristo dentre os mortos, 1 Pedro 1.3",
+r_text[84] = "🙏👉E o próprio nosso Senhor Jesus Cristo e nosso Deus e Pai, que nos amou, e em graça nos deu uma eterna consolação e boa esperança, 2 Tessalonicenses 2.16",
+r_text[85] = "🙏👉Tendo esperança em Deus, como estes mesmos também esperam, de que há de haver ressurreição de mortos, assim dos justos como dos injustos. Atos 24.15",
+r_text[86] = "🙏👉Manda aos ricos deste mundo que não sejam altivos, nem ponham a esperança na incerteza das riquezas, mas em Deus, que abundantemente nos dá todas as coisas para delas gozarmos. 1 Timóteo 6.17",
+r_text[87] = "🙏👉Que naquele tempo estáveis sem Cristo, separados da comunidade de Israel, e estranhos às alianças da promessa, não tendo esperança, e sem Deus no mundo. Efésios 2.12"
+
+
+
+]
+    var i = Math.floor(88*Math.random())
+    document.getElementById("sorte").innerHTML = "<p>" + r_text[i] + "</p>";
+    
+
+
+
+   write ('Olá, ' + mensagem + r_text[i]     + url1   )
+   var zap = "https://api.whatsapp.com/send?text="
+   var url =  window.open( zap +  ('Olá  ' )    +mensagem  +r_text[i]  + ('🙏👉')  +  url1  ) 
+
+     
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+    
+    
+    
+    
